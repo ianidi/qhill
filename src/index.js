fcc821da5c210bd85489629815ab67ade2399f5e
ianidi <51965970+ianidi@users.noreply.github.com>
2022-07-14 18:18:13 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureRootTheme } from "@yandex/ui/Theme";
+import { theme } from "@yandex/ui/Theme/presets/default";
+import App from "./App";
+import { store } from "./app/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import "reactjs-popup/dist/index.css";
+import "react-popper-tooltip/dist/styles.css";
+
+configureRootTheme({ theme });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>
 );
