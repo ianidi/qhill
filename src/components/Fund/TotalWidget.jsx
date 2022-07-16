0bf1c42b41bbf76bf8adb133c2b468c46771ddf1
ianidi <51965970+ianidi@users.noreply.github.com>
2022-07-16 20:30:27 +0300

    1

--- src/components/Fund/TotalWidget.jsx
+++ src/components/Fund/TotalWidget.jsx
@@ -4,6 +4,12 @@ import styled from "styled-components";
 import { flexGap } from "../../Common/FlexGap";
 import Select from "../UI/Select";
 
+import { ReactComponent as AUMImage } from "../../Asset/Images/aum.svg";
+import { ReactComponent as CommissionImage } from "../../Asset/Images/commission.svg";
+import { ReactComponent as InvestorsImage } from "../../Asset/Images/investors.svg";
+import { ReactComponent as PositiveImage } from "../../Asset/Images/positive.svg";
+import { ReactComponent as NegativeImage } from "../../Asset/Images/negative.svg";
+
 const TotalWidget = () => (
   <Container>
     <TitleContainer>
@@ -17,9 +23,35 @@ const TotalWidget = () => (
       />
     </TitleContainer>
     <WidgetContainer>
-      <Widget>hi</Widget>
-      <Widget>hi1</Widget>
-      <Widget>hi23</Widget>
+      <AUMWidget>
+        <AUMImage />
+        <WidgetTitle>
+          AUM <WidgetCurrency>(USDT)</WidgetCurrency>
+        </WidgetTitle>
+        <WidgetValue>121212</WidgetValue>
+        <WidgetGrowth
+          value={444}
+          caption="USDT"
+          containerStyle={{ background: "#fff" }}
+        >
+          this month
+        </WidgetGrowth>
+      </AUMWidget>
+      <Widget>
+        <CommissionImage />
+        <WidgetTitle>
+          Сommission <WidgetCurrency>(USDT)</WidgetCurrency>
+        </WidgetTitle>
+        <WidgetValue>6969</WidgetValue>
+      </Widget>
+      <Widget>
+        <InvestorsImage />
+        <WidgetTitle>Investors</WidgetTitle>
+        <WidgetValue>55</WidgetValue>
+        <WidgetGrowth value={8} caption="investors" negative>
+          this month
+        </WidgetGrowth>
+      </Widget>
     </WidgetContainer>
   </Container>
 );
@@ -48,23 +80,103 @@ const WidgetContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  align-items: flex-start;
   ${flexGap("20px")}
 `;
 const Widget = styled.div`
   display: flex;
+  flex-direction: column;
   min-height: 100px;
   max-width: 320px;
   min-width: 300px;
   align-items: flex-start;
   flex: 1;
-  background-color: #be0b0b;
+  padding: 32px;
+  background: #ffffff;
+  border: 2px solid #f4f4f4;
+  border-radius: 12px;
   @media screen and (max-width: 767px) {
     width: 100%;
     max-width: none;
     min-width: auto;
     flex: 0 auto;
   }
+`;
+const AUMWidget = styled(Widget)`
+  background: #efedfd;
+  border: 2px solid #efedfd;
+`;
+const WidgetTitle = styled.div`
+  margin-top: 16px;
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 16px;
+  letter-spacing: -0.01em;
+  color: #33383f;
+`;
+const WidgetCurrency = styled.span`
+  color: #6f767e;
+`;
+const WidgetValue = styled.div`
+  margin-top: 4px;
+  font-weight: 600;
+  font-size: 48px;
+  line-height: 48px;
+  letter-spacing: -0.03em;
+  color: #1a1d1f;
+`;
+
+const WidgetGrowth = ({
+  children,
+  value,
+  caption,
+  negative = false,
+  containerStyle = {},
+}) => {
+  return (
+    <WidgetGrowthContainer style={containerStyle}>
+      {!negative ? (
+        <React.Fragment>
+          <PositiveImage />
+          <WidgetValuePositive>
+            {value} {caption}
+          </WidgetValuePositive>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <NegativeImage />
+          <WidgetValueNegative>
+            {value} {caption}
+          </WidgetValueNegative>
+        </React.Fragment>
+      )}
+      &nbsp;{children}
+    </WidgetGrowthContainer>
+  );
+};
+const WidgetGrowthContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 4px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-left: 8px;
+  padding-right: 8px;
+  border-radius: 4px;
+  background: #f4f4f4;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: -0.01em;
+  color: #6f767e;
+`;
+const WidgetValuePositive = styled.span`
+  margin-left: 4px;
+  color: #83bf6e;
+`;
+const WidgetValueNegative = styled.span`
+  margin-left: 4px;
+  color: #d53a3a;
 `;
 
 export default TotalWidget;
