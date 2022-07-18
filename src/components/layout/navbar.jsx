56ddab72132eccdee484c6f20e53fae77ac58a1b
ianidi <51965970+ianidi@users.noreply.github.com>
2022-07-18 23:52:40 +0300

    1

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,20 +1,58 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Blockies from "react-blockies";
+import { useNavigate } from "react-router-dom";
+
 import WalletModal from "../Modal/WalletModal";
+import Select from "../UI/Select";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   const [visible, setVisible] = useState(false);
+
   return (
-    <Container>
-      <div onClick={() => setVisible(true)}>Open menu</div>
+    <React.Fragment>
+      <Container>
+        <div onClick={() => setVisible(true)}>
+          <b>Click</b> to connect wallet
+        </div>
+        <RightContainer>
+          <Select
+            //options={options.brand}
+            placeholder="EN"
+            //value={brand}
+            name="lng"
+            //setValue={(val) => dispatch(setBrand(val))}
+          />
+          <BlockiesContainer onClick={() => navigate("/profile")}>
+            <Blockies seed="Jeremy" size={16} scale={3} />
+          </BlockiesContainer>
+        </RightContainer>
+      </Container>
       <WalletModal visible={visible} setVisible={setVisible} />
-    </Container>
+    </React.Fragment>
   );
 };
 
 const Container = styled.div`
   display: flex;
+  width: 100%;
   align-items: center;
+  justify-content: space-between;
+`;
+const RightContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const BlockiesContainer = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-left: 20px;
+  cursor: pointer;
 `;
 
 export default Navbar;
